397ea63944fd033855cc55cfcca7a113f02f09b3
Alexandre Allaire <alexandre.allaire@mail.mcgill.ca>
2018-01-18 19:14:05 -0500

    Make whitespace consistent.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -18,45 +18,45 @@ KalmanFilter::KalmanFilter() {
 KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Predict(double delta_T) {
-    F_ << 1, 0, delta_T , 0,
-          0, 1, 0, delta_T,
-          0, 0, 1, 0,
-          0, 0, 0, 1;
+  F_ << 1, 0, delta_T , 0,
+        0, 1, 0, delta_T,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
 
-    Q_ << pow(delta_T, 4)/4* sig_a, 0, pow(delta_T, 3)/2*sig_a, 0,
-         0, pow(delta_T, 4)/4* sig_a, 0, pow(delta_T, 3)/2*sig_a,
-         pow(delta_T, 3)/2*sig_a, 0, pow(delta_T, 2) * sig_a, 0,
-         0, pow(delta_T, 3)/2*sig_a, 0, pow(delta_T, 2) * sig_a;
+  Q_ << pow(delta_T, 4)/4* sig_a, 0, pow(delta_T, 3)/2*sig_a, 0,
+       0, pow(delta_T, 4)/4* sig_a, 0, pow(delta_T, 3)/2*sig_a,
+       pow(delta_T, 3)/2*sig_a, 0, pow(delta_T, 2) * sig_a, 0,
+       0, pow(delta_T, 3)/2*sig_a, 0, pow(delta_T, 2) * sig_a;
 
-    x_ = F_ * x_;
-    P_ = F_ * P_ * F_.transpose() + Q_;
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-    update(z - H_ * x_);
+  update(z - H_ * x_);
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
 
-    // Map predicted state into polar coordinates.
-    double px = x_(0), py = x_(1), vx = x_(2), vy = x_(3);
-    auto polar = VectorXd(3);
-    polar(0) = sqrt((px*px) + py*py);
-    polar(1) = atan2(py, px);
-    polar(2) = (px*vx + py*vy) / dbz_guard(sqrt(px*px+py*py));
+  // Map predicted state into polar coordinates.
+  double px = x_(0), py = x_(1), vx = x_(2), vy = x_(3);
+  auto polar = VectorXd(3);
+  polar(0) = sqrt((px*px) + py*py);
+  polar(1) = atan2(py, px);
+  polar(2) = (px*vx + py*vy) / dbz_guard(sqrt(px*px+py*py));
 
-    VectorXd innovation = z - polar;
+  VectorXd innovation = z - polar;
 
-    // Normalize angles.
-    innovation(1) = atan2(sin(innovation(1)), cos(innovation(1)));
+  // Normalize angles.
+  innovation(1) = atan2(sin(innovation(1)), cos(innovation(1)));
 
-    update(innovation);
+  update(innovation);
 }
 
 void KalmanFilter::update(const VectorXd &innovation) {
-    MatrixXd S = H_ * P_ * H_.transpose() + R_;
-    MatrixXd K = P_ * H_.transpose() * S.inverse();
+  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd K = P_ * H_.transpose() * S.inverse();
 
-    x_ += K * innovation;
-    P_ -= K * H_ * P_;
+  x_ += K * innovation;
+  P_ -= K * H_ * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -8,9 +8,9 @@ using std::vector;
 // Can still result in INF.
 double dbz_guard(double x)
 {
-    if (x == 0.0)
-        return 0.000000001;
-    return x;
+  if (x == 0.0)
+      return 0.000000001;
+  return x;
 }
 
 Tools::Tools() {}
@@ -19,29 +19,29 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-    auto sum = VectorXd(4);
-    sum << 0, 0, 0, 0;
+  auto sum = VectorXd(4);
+  sum << 0, 0, 0, 0;
 
-    for (int i = 0; i < estimations.size(); i++) {
-        VectorXd diff = estimations[i] - ground_truth[i];
-        diff = diff.array() * diff.array();
-        sum += diff;
-    }
+  for (int i = 0; i < estimations.size(); i++) {
+    VectorXd diff = estimations[i] - ground_truth[i];
+    diff = diff.array() * diff.array();
+    sum += diff;
+  }
 
-    return (sum.array() / estimations.size()).sqrt();
+  return (sum.array() / estimations.size()).sqrt();
 }
 
 // Calculate the jacobian of the state-to-measurement function with respect to state.
 MatrixXd Tools::CalculateJacobian(const VectorXd& x) {
 
-    double px = x(0), py = x(1), vx = x(2), vy = x(3);
+  double px = x(0), py = x(1), vx = x(2), vy = x(3);
 
-    auto J = MatrixXd(3, 4);
+  auto J = MatrixXd(3, 4);
 
-    J << px/dbz_guard(sqrt(px*px+py*py)), py/dbz_guard(sqrt(px*px+py*py)), 0, 0,
-         -py/dbz_guard(px*px+py*py), px/dbz_guard(px*px+py*py), 0, 0,
-         py*(vx*py-vy*px)/dbz_guard(pow(px*px+py*py, 3.0/2.0)), px*(vy*px-vx*py)/dbz_guard(pow(px*px+py*py, 3.0/2.0)),
-         px/dbz_guard(sqrt(px*px+py*py)), dbz_guard(py/sqrt(px*px+py*py));
+  J << px/dbz_guard(sqrt(px*px+py*py)), py/dbz_guard(sqrt(px*px+py*py)), 0, 0,
+       -py/dbz_guard(px*px+py*py), px/dbz_guard(px*px+py*py), 0, 0,
+       py*(vx*py-vy*px)/dbz_guard(pow(px*px+py*py, 3.0/2.0)), px*(vy*px-vx*py)/dbz_guard(pow(px*px+py*py, 3.0/2.0)),
+       px/dbz_guard(sqrt(px*px+py*py)), dbz_guard(py/sqrt(px*px+py*py));
 
-    return J;
+  return J;
 }
